adf715218bcdbb7101a5bfae56a0e3c2b4c5b34f
Chadrik Bertrand <2037079@cstj.qc.ca>
2023-10-23 13:00:16 -0400

    feature-extend-img

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     screens: {
-      'xl': '1200px',
+      xl: "1200px",
     },
-    extend: {    },
+    extend: {
+      backgroundImage: {
+        "fond-breuvage": 'url("src/assets/19326390.jpg")',
+        "fond-entree": 'url("/src/assets/images/fond-breuvage.jpg")',
+        "fond-plats": 'url("/src/assets/images/fond-breuvage.jpg")',
+        "fond-dessert": 'url("/src/assets/images/fond-breuvage.jpg")',
+      },
+    },
   },
   plugins: [],
-}
-
+};
