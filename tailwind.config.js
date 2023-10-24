b44c78735faeeb9222de020d02ddb0c58b22ddae
Chadrik Bertrand <2037079@cstj.qc.ca>
2023-10-24 09:08:42 -0400

    fix-couleur-et-font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,13 @@ export default {
     screens: {
       xl: "1200px",
     },
+    fontFamily:{
+      'cool':['Coolvetica', 'sans-serif'],
+      'butler':['Butler', 'sans-serif'],
+    },
     extend: {
       backgroundImage: {
-        "fond-breuvage": 'url("src/assets/19326390.jpg")',
+        "fond-breuvage": 'url("src/assets/fond-breuvage.jpg")',
         "fond-entree": 'url("/src/assets/images/fond-breuvage.jpg")',
         "fond-plats": 'url("/src/assets/images/fond-breuvage.jpg")',
         "fond-dessert": 'url("/src/assets/images/fond-breuvage.jpg")',
@@ -15,6 +19,33 @@ export default {
       rotate: {
         270: "270deg",
       },
+      colors:{
+      'orange':{
+        'pale': '#F08400',
+        'reg': '#D57500',
+        'fonce': '#BD6800'
+      },
+      'rouge':{
+        'pale': '#A84520',
+        'reg':'#8F3B1B',
+        'fonce': '#753016'
+      },
+      'jaune':{
+        'pale':'#E0A510',
+        'reg':'#C8930E',
+        'fonce':'#AD7F0C'
+      },
+      'bleu':{
+        'pale':'#60778C',
+        'reg':'#4E6172',
+        'fonce':'#3D4C59'
+      },
+      'gris':{
+        'pale':'#4D5D66',
+        'reg':'#3A464D',
+        'fonce':'#262E33'
+      }
+      }
     },
   },
   plugins: [],
