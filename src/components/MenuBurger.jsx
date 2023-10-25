9c572f63450e80c887af47b596e0faa63b1a2041
Chadrik Bertrand <2037079@cstj.qc.ca>
2023-10-25 11:52:25 -0400

    feature-animation-menu-burger

--- src/components/MenuBurger.jsx
+++ src/components/MenuBurger.jsx
@@ -1,11 +1,18 @@
 import "./menu-burger.css";
+import { useState } from "react";
 const MenuBurger = () => {
+  const [open, setOpen] = useState(false);
+  const toggleButton = () => {
+    setOpen(!open);
+    console.log(open);
+  };
   return (
-    <svg viewBox="0 0 150 100" width="50" height="50" className="menu-burger absolute ">
-      <rect width="150" height="20" rx="10"></rect>
-      <rect y="50" width="150" height="20" rx="10"></rect>
-      <rect y="100" width="150" height="20" rx="10"></rect>
-    </svg>
+    <label className={"burger"} htmlFor="burger">
+      <input type="checkbox" id="burger" />
+      <span></span>
+      <span></span>
+      <span></span>
+    </label>
   );
 };
 export default MenuBurger;
