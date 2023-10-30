6c8e2c37571b7c5c21688eaa823e6c538a171eb7
Chadrik Bertrand <2037079@cstj.qc.ca>
2023-10-29 21:38:56 -0400

    feature-backhand-menu

--- src/components/MenuBurger.jsx
+++ src/components/MenuBurger.jsx
@@ -2,12 +2,16 @@ import "./menu-burger.css";
 import { useState } from "react";
 const MenuBurger = () => {
   const [open, setOpen] = useState(false);
-  const toggleButton = () => {
+  const toggleButton = (e) => {
+    e.preventDefault();
     setOpen(!open);
     console.log(open);
+    const burger = document.querySelector(".burger");
+    burger.classList.toggle("animate");
   };
+
   return (
-    <label className={"burger"} htmlFor="burger">
+    <label className={`burger ${open ? 'animate':''}`} htmlFor="burger" onClick={toggleButton}>
       <input type="checkbox" id="burger" />
       <span></span>
       <span></span>
