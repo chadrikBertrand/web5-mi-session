6e1bf9ca7715bec9818104f79b851f899342990f
Chadrik Bertrand <2037079@cstj.qc.ca>
2023-10-29 22:01:47 -0400

    feature-slider-menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,33 @@
+import { Link } from "react-router-dom";
 import "./Header.css";
 import MenuBurger from "./MenuBurger";
 import logo from "/src/assets/logo.svg";
+import { useState } from "react";
 const Header = () => {
+  const [open, setOpen] = useState(false);
+  const receiveChildValue = (value) => {
+    setOpen(value);
+    console.log(value);
+  };
+
   return (
     <header className="h-full flex flex-row items-center place-content-between">
       <div className="logo">
         <img src={logo} alt="logo" className="m-auto w-full h-44 p-8" />
       </div>
 
-      <MenuBurger />
+      <MenuBurger sendValueToParent={receiveChildValue} />
+      <div
+        className="slider-menu absolute top-0 left-0 h-screen w-full z-50
+      bg-white flex flex-col items-center justify-center text-4xl text-rouge-pale"
+      >
+        <Link to="/authentification" className="">
+          Profile
+        </Link>
+        <Link to="/menu" className="">
+          Menu
+        </Link>
+      </div>
     </header>
   );
 };
--- src/components/MenuBurger.jsx
+++ src/components/MenuBurger.jsx
@@ -1,10 +1,11 @@
 import "./menu-burger.css";
 import { useState } from "react";
-const MenuBurger = () => {
+const MenuBurger = (sendValueToParent) => {
   const [open, setOpen] = useState(false);
   const toggleButton = (e) => {
     e.preventDefault();
     setOpen(!open);
+    sendValueToParent(open);
     console.log(open);
     const burger = document.querySelector(".burger");
     burger.classList.toggle("animate");
